24233408257705e7aad67ebc723a274a918c5ef2
wuyl <2331336537@qq.com>
2020-06-02 23:05:17 +0800

    feat:init

---
+++ bin/rain-pack.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+
+const path = require('path')
+
+const config = require(path.resolve('webpack.config.js'))
+
+const Compiler = require('../lib/Compiler.js')
+const compiler = new Compiler(config)
+
+// 运行编译
+compiler.run()
